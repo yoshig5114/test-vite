d745f735ddf3729cdf4bc78b18f271842c3b1ffe
Joshua Gregory <yoshig5114@gmail.com>
2023-06-07 17:08:22 -0400

    added navbar to app.jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar(){
+    return(
+        <div>
+            <ul>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>CONTACT</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
